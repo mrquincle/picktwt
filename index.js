31a7e42b10fb9913c00223f4abe92111b28cf737
Anne van Rossum <anne@crownstone.rocks>
2017-10-07 20:29:29 +0200

    Help people construct a social card.

--- index.js
+++ index.js
@@ -6,10 +6,23 @@ var port = process.env.PORT || 4000;
 
 app.set('view engine', 'ejs');
 
+app.use("/static", express.static(__dirname + '/static'));
+
 app.get('/query', function(req, res) {
-	console.log(req.query.picture);
-//    res.sendFile(path.join(__dirname + '/index.html'));
-    res.render('index.ejs', { query: req.query });
+    console.log("Serve actual request");
+    console.log(req.query);
+    res.render('query.ejs', { query: req.query });
+});
+
+app.get('/display', function(req, res) {
+    console.log("Show URL that can be used");
+    console.log(req.query);
+    res.render('display.ejs', { query: req.query });
+});
+
+app.get('/', function(req, res) {
+    console.log("Main page");
+    res.render('index.ejs');
 });
 
 app.listen(port);
